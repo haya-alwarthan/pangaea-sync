f39c1eb408958954c374b11aaf2e3a73397d7129
haya-alwarthan <111225040+haya-alwarthan@users.noreply.github.com>
2023-10-08 00:47:23 +0300

    prepare for github pages deployment

---
+++ src/lib/sections/About.svelte
@@ -0,0 +1,17 @@
+<section id="#about" class="h-screen p-10 py-[8rem]">
+
+<h1 class="font-bold text-white text-5xl">Future plans</h1>
+<ul class="text-white text-2xl">
+    <li class="my-3">More data from multiple paltforms</li>
+    <li class="my-3">Advanced search algorithm based on NLP and similarty index</li>
+    <li class="my-3">Provide a customized descreption for any data result</li>
+</ul>
+
+
+<h1 class="font-bold text-white text-5xl my-20">Business Value</h1>
+<ul class="text-white text-2xl">
+    <li class="my-3">API Services: Hosting the app in cloud to be provided as a Platform as a Service </li>
+    <li class="my-3">Integration with environmental related agencies</li>
+    <li class="my-3">Providing a community for initiatives built in our platform  </li>
+</ul>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
   import Hero from "$lib/sections/Hero.svelte";
   import SwitchButton from "../lib/components/SwitchButton.svelte";
+  import About from "../lib/sections/About.svelte";
   import ExploreData from "../lib/sections/ExploreData.svelte";
   import WorldMap from "../lib/sections/WorldMap.svelte";
 </script>
 
 <Hero />
+<About/>
 <ExploreData/>
 <WorldMap/>
 
