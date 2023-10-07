aafe89f2c3a32b754a8de0dfb917d6a283dbb7a1
haya-alwarthan <111225040+haya-alwarthan@users.noreply.github.com>
2023-10-08 01:11:37 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev')
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   
@@ -7,7 +9,7 @@ const config = {
     adapter: adapter(),
     
     paths:{
-      base:process.env.NODE_ENV === 'production'?'/sveltekit-github-pages':''
+      base:dev?'':process.env.BASE_PATH
     }
   },
   preprocess: vitePreprocess()
